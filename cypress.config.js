980e8e3b8692642f36a3ee920a30f585bc6f10e4
Francielli Pinheiro Dias <francielli_dias@terceiros.sicredi.com.br>
2024-05-23 16:12:57 -0300

    tentativa de adicionar allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'cypress';
+import { allureCypress } from 'allure-cypress/reporter';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://buger-eats.vercel.app/'
+    baseUrl: 'https://buger-eats.vercel.app/',
+    setupNodeEvents(on, config) {
+      allureCypress(on);
+      },
   },
 });
